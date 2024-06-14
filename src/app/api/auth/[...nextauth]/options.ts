0db781b31d90ef400bf96e9b55f3c5ac237b5bc2
Andrii <butsaandrii@gmail.com>
2024-06-14 14:28:28 +0300

    feat: config for next-auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,39 @@
+import type { NextAuthOptions } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const options: NextAuthOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username:",
+                    type: "text",
+                    placeholder: "your-cool-username"
+                },
+                password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: "your-awesome-password"
+                }
+            },
+            async authorize(credentials) {
+                // This is where you need to retrieve user data 
+                // to verify with credentials
+                // Docs: https://next-auth.js.org/configuration/providers/credentials
+                const [userId,userName,userPassword] = process.env.DUMMY_USER?.split(",") as string[];
+
+                if (credentials?.username === userName && credentials?.password === userPassword) {
+                    return {id:userId,userName,userPassword}
+                } else {
+                    return null
+                }
+            }
+        })
+    ],
+}
